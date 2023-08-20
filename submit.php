b67627e2ef0e58bf5e29fbbd3eb9dc7c16253504
Dheephiga <64630313+dheephiga@users.noreply.github.com>
2023-08-20 19:06:01 +0530

    adding changes

---
+++ submit.php
@@ -0,0 +1,37 @@
+<?php
+
+$env_file_path = realpath("/.env");
+
+$servername = $_ENV['servername'];
+$username = $_ENV['username'];
+$password = $_ENV['password'];
+$dbname = $_ENV['dbname'];
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if($conn->connect_error)
+{
+    die("Connection Failed : ".$conn->connect_error);
+}
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$sql = "INSERT INTO contact_form(name, email, subject, message) VALUES ('$name','$email','$subject','$message')";
+
+if($conn->query($sql)==TRUE)
+{
+    echo "Message sent successfully!";
+}
+
+else
+{
+    echo "Error: ".$sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
+?>
+
